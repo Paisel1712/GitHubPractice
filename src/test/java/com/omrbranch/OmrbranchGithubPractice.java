404623f61f6f0eb7ba4864455eea9525dfaafe21
Paisel1712 <mohammedpaisel1712@gmail.com>
2025-01-06 17:16:43 +0530

    pushing to master

--- src/test/java/com/omrbranch/OmrbranchGithubPractice.java
+++ src/test/java/com/omrbranch/OmrbranchGithubPractice.java
@@ -1,5 +1,13 @@
 package com.omrbranch;
 
 public class OmrbranchGithubPractice {
-
+	public void method1()
+	{
+		System.out.println(1);
+	}
+	public void method2()
+	{
+		System.out.println(2);
+	}
+	
 }
